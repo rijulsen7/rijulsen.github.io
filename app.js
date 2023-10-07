41c1e5651cfcdbbfc420246e69212e2529f53cc2
Rijul sen <68010035+rijulsen7@users.noreply.github.com>
2023-10-07 21:47:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+
+// <!-- typed js effect starts -->
+var typed = new Typed(".typing-text", {
+    strings: ["Student 🧑", "Avid Reader 📖", "Hustler  🚵", "Adventurous Spirit 🗺️", ""],
+    loop: true,
+    typeSpeed: 40,
+    backSpeed: 15,
+    backDelay: 500,
+  });
+  // <!-- typed js effect ends --> 
