4ba51dcb7345828e4741ed01fbd936f2e82fb4a3
Rijul sen <68010035+rijulsen7@users.noreply.github.com>
2023-12-19 18:47:07 +0530

    y

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+// <!-- typed js effect starts -->
+var typed = new Typed(".typing-text", {
+  strings: ["Student 🧑", "Avid Reader 📖", "Hustler  🚵", "Adventurous Spirit 🗺️", ""],
+  loop: true,
+  typeSpeed: 40,
+  backSpeed: 15,
+  backDelay: 500,
+});
+// <!-- typed js effect ends --> 
